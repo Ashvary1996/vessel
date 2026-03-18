50bac7c8bde163d2be50bb945c45c25253eddf61
Ashvary1996 <ashvarygidian1996@gmail.com>
2026-03-18 17:29:46 +0530

    testing ci/cd set up

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ const port = 8000;
 app.get("/", (req, res) => {
   res.send("Hello, World from linux !.......");
 });
+app.get("/cidi", (req, res) => {
+  res.send("cicd testing");
+});
 app.get("/status", (req, res) => {
   res.json({
     status: "ok",
-    message:`Server is healthy and running at port 8000 with pid ${process.pid}`
+    message: `Server is healthy and running at port 8000 with pid ${process.pid}`
   });
 });
+ 
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
