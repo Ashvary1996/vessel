cf768c08a95cef7da4932abf0d67929caf18a1bb
Ashvary1996 <ashvarygidian1996@gmail.com>
2026-03-18 17:54:14 +0530

    off cicd

--- server/index.js
+++ server/index.js
@@ -5,18 +5,22 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
+  console.log("Hello, World from linux !.......");
+
   res.send("Hello, World from linux !.......");
 });
 app.get("/cidi", (req, res) => {
+  console.log("cicd testing");
   res.send("cicd testing");
 });
 app.get("/status", (req, res) => {
+  console.log("Server is healthy and running at port 8000 with pid ${process.pid}");
   res.json({
     status: "ok",
     message: `Server is healthy and running at port 8000 with pid ${process.pid}`
   });
 });
- 
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
